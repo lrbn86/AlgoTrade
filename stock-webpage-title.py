b278542f48882ba914dc717483e3972dd2edbbcd
Brandon Nguyen <lrbn86@gmail.com>
2019-12-10 17:54:13 -0500

    Created a stock monitor

--- stock-webpage-title.py
+++ stock-webpage-title.py
@@ -1,25 +1,61 @@
-# Grabs stock price from Yahoo Finance on their title.
+# Grabs stock price from Yahoo Finance 
+
 import urllib
 from bs4 import BeautifulSoup
-stock_name = "^IXIC"
-url = "https://finance.yahoo.com/quote/" + stock_name
+from datetime import datetime
+from dateutil import tz
+from pytz import timezone
+import time
 
-def getPrice():
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    m = soup.find_all('span')
-    print(m)
-    m = m [13:16]
-    stock_price = m[0].string
-    print(stock_price)
+capital = 10000
 
-#while True:
-#    getPrice()
+# Eastern Time Zone
+MARKET_TIME_CLOSE = "04:00 PM"
 
-while True:
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    m = soup.find(id="quote-header-info").descendants
-    n = [d.text for d in m if d.name == "span"]
-    stock_price = n[1]
-    print(stock_price)
+STOCK_NAME = "JPY=X"
+
+STOCK_URL = "https://finance.yahoo.com/quote/" + STOCK_NAME
+
+# Get time by first getting UTC and converting to EST
+# Used to determine when to stop trading, i.e. stop trading when market closes
+def getTime():
+    utc = timezone('UTC')
+    now = utc.localize(datetime.utcnow())
+    est = timezone('US/Eastern')
+    local_time = now.astimezone(est).strftime("%H:%M:%S")
+    d = datetime.strptime(local_time, "%H:%M:%S").strftime("%I:%M:%S %p")
+    return d
+
+def getStockInfo():
+    STOCK_URL_PAGE = urllib.request.urlopen(STOCK_URL)
+    html = BeautifulSoup(STOCK_URL_PAGE, 'html.parser')
+    MAIN_ID = html.find(id="quote-header-info").descendants
+    STOCK_INFO = [stock_info.text for stock_info in MAIN_ID if stock_info.name == "span"]
+    stock_price = STOCK_INFO[1]
+    print("\t"+stock_price + " @ " + getTime())
+
+
+def strategy1(stock_price):
+    print("Do strategy here")
+
+def startSimulation():
+    while True:
+        getStockInfo()
+        time.sleep(0.1) # Give cpu time to rest
+
+def startTrade():
+    # The main loop
+    while getTime() < market_time_close:
+        getTime()
+        time.sleep(0.1) # Give cpu time to rest
+    print("Market Closed @ " + market_time_close)
+
+
+print("\n########################################################################\n")
+print("\tWelcome to the Stock Price Monitor")
+print("\tThe stock of interest is [ " + STOCK_NAME + " ] ")
+print("\tLooking at stock price now...\n")
+time.sleep(5)
+startSimulation()
+
+
